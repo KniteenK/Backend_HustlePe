ffe5ea769eaaf879cc0ac49e1e1c232fcb791a51
Niketiiitd <niket22320@iiitd.ac.in>
2024-11-21 17:24:37 +0530

    Update gig.controller.js

--- HustlePe-backend/src/controllers/gig.controller.js
+++ HustlePe-backend/src/controllers/gig.controller.js
@@ -1,8 +1,9 @@
 import { gigs } from "../models/gigs.model.js";
+import apiResponse from "../utils/apiResponse.js";
 
-const getGigs = async () => {
+const getGigs = async (req,res) => {
     const { skillsArray, sortBy = 'createdAt', order = -1, page = 1, limit = 10} = req.body;
-    
+    console.log (skillsArray, sortBy, order, page, limit);
     try {
       const sortOptions = {};
       sortOptions[sortBy] = order;
@@ -11,8 +12,16 @@ const getGigs = async () => {
         .sort(sortOptions)
         .skip((page - 1) * limit)
         .limit(limit);
+
+        console.log(jobs);
   
-      return jobs;
+      return res.status(200).json(
+        new apiResponse(
+          200,
+          jobs,
+          "Jobs fetched successfully",
+        )
+      );
     } catch (error) {
       console.error(error);
       throw new Error('Error fetching jobs by skills and filter');
